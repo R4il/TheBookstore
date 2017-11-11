cab22188e5a689804897441e22abcefcee0eca73
Gabriel Clark <gaboperezclark@gmail.com>
2017-11-11 00:22:33 -0500

    Added wishlist, and delte wishlist and cart items function

--- purchases/urls.py
+++ purchases/urls.py
@@ -9,6 +9,16 @@ urlpatterns = [
     # /cart/checkout/
     url(r'checkout/', checkout, name='checkout'),
     
+    # /cart/wishlist/id/
+    url(r'wishlist/drop/(?P<book_id>[0-9]+)/', dropWishlist, name='dropWishlist'),
+    
+    # /cart/checkout/
+    url(r'wishlist/', displayWishlist, name='displayWishlist'),
+    
+    # /cart/checkout/
+    url(r'drop/(?P<book_id>[0-9]+)/', drop, name='drop'),
+    
+    
     # /cart/
     url(r'', displayCart, name='displayCart'),
     
--- purchases/views.py
+++ purchases/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views.generic import DeleteView, CreateView, RedirectView, FormView, UpdateView
+from django.core.urlresolvers import reverse_lazy, reverse
 from .models import *
 # Create your views here.
 
@@ -26,6 +28,15 @@ def displayCart(request):
     return render(request, 'displayCart.html', {'cart': userCart})
 
 
+def displayWishlist(request):
+    user = request.user.user_id
+    wishlist = WishList.objects.filter(user_id=user)
+    for item in wishlist:
+        itemPrice = item.book.price * item.qty
+        item.price = itemPrice
+    return render(request, 'displayWishlist.html', {'wishlist': wishlist})
+
+
 def checkout(request):
     user = request.user.user_id
     userCart = Orders.objects.filter(user_id=user)
@@ -35,3 +46,21 @@ def checkout(request):
         order.save()
     userCart.delete()
     return HttpResponseRedirect('/books')
+
+
+def drop(request, book_id):
+    if request.user.is_anonymous:
+        return HttpResponseRedirect('/accounts/signUp/')
+    if book_id:
+        entry = Orders.objects.get(user=request.user, book_id=book_id)
+        entry.delete()
+    return HttpResponseRedirect(f'/cart')
+
+
+def dropWishlist(request, book_id):
+    if request.user.is_anonymous:
+        return HttpResponseRedirect('/accounts/signUp/')
+    if book_id:
+        entry = WishList.objects.get(user=request.user, book_id=book_id)
+        entry.delete()
+    return HttpResponseRedirect(f'/cart/wishlist')
